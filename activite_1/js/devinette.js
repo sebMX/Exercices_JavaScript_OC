31424c11eb17d2c85cc0c7cb6dee058d5ca80d76
sebMX <seb.massonneau@gmail.com>
2018-06-21 18:58:29 +0200

    Ajout contenu

--- activite_1/js/devinette.js
+++ activite_1/js/devinette.js
@@ -1,32 +1,41 @@
-// Exercice : devinette
+/* 
+Activité : jeu de devinette
+*/
 
-// Message de bienvenue
+// NE PAS MODIFIER OU SUPPRIMER LES LIGNES CI-DESSOUS
+// COMPLETEZ LE PROGRAMME UNIQUEMENT APRES LE TODO
+
 console.log("Bienvenue dans ce jeu de devinette !");
 
-// Génère un nombre entre 1 et 100 en nombre entier
-var resultat = Math.floor(Math.random() * 100) + 1;
+// Cette ligne génère aléatoirement un nombre entre 1 et 100
+var solution = Math.floor(Math.random() * 100) + 1;
+
+// Décommentez temporairement cette ligne pour mieux vérifier le programme
+//console.log("(La solution est " + solution + ")");
+
+// TODO : complétez le programme
 
 // Initialise des variables
 var nombre = 0, nombreTentative = 0; 
 
-while ((nombre !== resultat) && (nombreTentative <= 6)) {
+while ((nombre !== solution) && (nombreTentative <= 6)) {
 		nombre = Number(prompt("Entrez un nombre entre 1 et 100 :"));	// Saisie du nombre par l'utilisateur (6 essais max)
 		nombreTentative++;		// Incrémente le nombre de tentative
 
 	if ((nombre < 1) || (nombre > 100) || isNaN(nombre)) {	// Vérifie si c'est bien un nombre (type) et bien entre 1 et 100
 		console.log("Saisie incorrecte, veuillez entrer un nombre entre 1 et 100");
 	}
-	else if (nombre < resultat) {	// Si nombre inférieur au résultat
+	else if (nombre < solution) {	// Si nombre inférieur au résultat
 		console.log(nombre + " est trop petit");
 	}
-	else if (nombre > resultat) {	// Si nombre supérieur au résultat
+	else if (nombre > solution) {	// Si nombre supérieur au résultat
 		console.log(nombre + " est trop grand");
 	}
 }
-	if ((nombre == resultat) && (nombreTentative <= 6)) {	// Si nombre égale au résultat avec 6 essais max
-		console.log("Bravo !! La solution est : " + resultat);
+	if ((nombre == solution) && (nombreTentative <= 6)) {	// Si nombre égale au résultat avec 6 essais max
+		console.log("Bravo !! La solution est : " + solution);
 		console.log("Vous avez trouvé en " + nombreTentative + " essai(s)");
 	}
 	else {	// Sinon perdu
-		console.log("Perdu... La solution était " + resultat);
+		console.log("Perdu... La solution était " + solution);
 	}
